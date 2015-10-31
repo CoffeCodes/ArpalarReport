9a77121ac4d3981bc1b3df71d17cf35170eafe72
CoffeCodes <whiteteenboy@gmail.com>
2015-11-01 00:53:49 +0630

    Added BuildVars

---
+++ app/src/main/java/com/arpalarreport/app/BuildVars.java
@@ -0,0 +1,19 @@
+package com.arpalarreport.app;
+
+/**
+ * Created by user on 11/1/15.
+ */
+
+public class BuildVars {
+  public static boolean DEBUG_VERSION = false;
+  public static int BUILD_VERSION = 654;
+  public static int APP_ID = 42408; //obtain your own APP_ID at https://core.telegram.org/api/obtaining_api_id
+  public static String APP_HASH = "07501bdbe9f09d6368f0f0e6c97661d1"; //obtain your own APP_HASH at https://core.telegram.org/api/obtaining_api_id
+  //public static String HOCKEY_APP_HASH = "your-hockeyapp-api-key-here";
+  //public static String GCM_SENDER_ID = "";
+  public static String SEND_LOGS_EMAIL = "yelinaung98765@gmail.com";
+  //public static String BING_SEARCH_KEY = ""; //obtain your own KEY at https://www.bing.com/dev/en-us/dev-center
+  //public static String FOURSQUARE_API_KEY = ""; //obtain your own KEY at https://developer.foursquare.com/
+  //public static String FOURSQUARE_API_ID = ""; //obtain your own API_ID at https://developer.foursquare.com/
+  //public static String FOURSQUARE_API_VERSION = "20150326";
+}
